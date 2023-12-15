3beb5307c65f6eebc97059fd1ef09b2657b8f1d6
zhibek17 <primovajibek@gmail.com>
2023-12-15 15:32:20 +0600

    Tracker app

---
+++ tracker/tracker_app/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Task, Status, Type
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'summary', 'description', 'status', 'type', 'created_at', 'updated_at']
+    list_filter = ['status', 'type']
+    search_fields = ['summary']
+
+
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    search_fields = ['name']
+
+
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    search_fields = ['name']
---
+++ tracker/tracker_app/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+
+
+class AbstractModel(models.Model):
+    created_at = models.DateTimeField(verbose_name='Время создания', auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+
+    class Meta:
+        abstract = True
+
+
+class Type(AbstractModel):
+    name = models.CharField(max_length=100, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+
+    def __str__(self):
+        return self.name
+
+
+class Status(AbstractModel):
+    name = models.CharField(max_length=100, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+
+    def __str__(self):
+        return self.name
+
+
+class Task(AbstractModel):
+    TYPE_CHOICES = (
+        ('Task', 'Задача'),
+        ('Bug', 'Ошибка'),
+        ('Enhancement', 'Улучшение')
+    )
+
+    STATUS_CHOICES = (
+        ('New', 'Новый'),
+        ('In Progress', 'В процессе'),
+        ('Done', 'Выполнено')
+    )
+
+    summary = models.CharField(max_length=100, blank=False, null=False, verbose_name='Краткое описание')
+    description = models.TextField(blank=True, null=True, verbose_name='Полное описание')
+    status = models.ForeignKey(Status, on_delete=models.PROTECT, verbose_name='Статус')
+    type = models.ForeignKey(Type, on_delete=models.PROTECT, verbose_name='Тип')
+
+    def __str__(self):
+        return self.summary
