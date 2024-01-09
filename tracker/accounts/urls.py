6705bddcc057162c4cc350987b998f8304f7c8ab
zhibek17 <primovajibek@gmail.com>
2024-01-09 18:42:04 +0600

    ссылки для регистрации

--- tracker/accounts/urls.py
+++ tracker/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from accounts.views import login_view, logout_view
 from django.contrib.auth.views import LoginView, LogoutView
+from accounts.views import RegisterView
 
 app_name = 'accounts'
 
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout')
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='user_create')
 ]
