ddb14d6c8c06d1fc39d870b74237cd8dd227d05b
zhibek17 <primovajibek@gmail.com>
2024-01-09 18:41:30 +0600

    модели для проектов

--- tracker/accounts/models.py
+++ tracker/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=400)
+    users = models.ManyToManyField(User, related_name='projects', through='ProjectMembership')
+
+class ProjectMembership(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    project = models.ForeignKey(Project, on_delete=models.PROTECT)
